ef984d162f336cde268cec852ac3cc191bb89cdf
mootensai <mootensai@users.noreply.github.com>
2015-05-04 17:46:46 +0700

    Update BeforeQueryTrait.php
    Implementing Once's Improvement on http://www.yiiframework.com/wiki/813/before-query-example-soft-delete/

--- base/BeforeQueryTrait.php
+++ base/BeforeQueryTrait.php
@@ -8,9 +8,10 @@ trait BeforeQueryTrait{
         $obj = new static;
         $class = new \ReflectionClass($obj);
         $condition = [];
-        foreach ($class->getProperties() as $property) {
-            if(strpos($property->getName(),'BEFORE_QUERY') !== false && is_array($property->getValue($obj)) && $property->isStatic())
+        foreach ($class->getProperties(\ReflectionProperty::IS_STATIC) as $property) {
+            if(strpos($property->getName(),'BEFORE_QUERY') !== false && is_array($property->getValue($obj))){
                 $condition = array_merge($condition, $property->getValue($obj));
+            }
         }
         return parent::find()->andFilterWhere($condition);
     }
