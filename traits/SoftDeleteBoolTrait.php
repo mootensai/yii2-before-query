36246cee87157e8bc1e3731c3f69746df4175313
Yohanes Candrajaya <mootensai@users.noreply.github.com>
2015-06-01 11:01:43 +0700

    Update SoftDeleteBoolTrait.php

--- traits/SoftDeleteBoolTrait.php
+++ traits/SoftDeleteBoolTrait.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ * SoftDeleteBoolTrait
+ * Flag the deleted row with 1(true).
+ * Search the deleted column with 0.
+ * The deleted column must 'deleted', 
+ * if you want to change this, change the property & default argument for $softDeleteColumn to your need
+ *
+ * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+ * @since 1.0
+ */
 namespace mootensai\beforequery\traits;
 
 trait SoftDeleteBoolTrait{
-    public static $BEFORE_QUERY_SOFT_DELETE = ['isdelete' => 0];
+    public static $BEFORE_QUERY_SOFT_DELETE = ['deleted' => 0];
     
     public function deleteSoft() {
         $this->{$this->BEFORE_QUERY_SOFT_DELETE} = 1;
